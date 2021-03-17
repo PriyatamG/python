a54dbcb44d05bbce23cedac85e5cbcaf43e77183
PriyatamG <53619165+PriyatamG@users.noreply.github.com>
2021-03-17 15:04:23 +0530

    Update HangmanSqlBeta.py

--- Main/HangmanSqlBeta.py
+++ Main/HangmanSqlBeta.py
@@ -1,15 +1,9 @@
 import mysql.connector as sql
 import random
 
-database = sql.connect(host = "localhost", user = "root", passwd = "", database = "world")
-if database.is_connected():
-	print("Succesfully connected")
-
-cursor = database.cursor()
 
 
-
-def canvas(word):
+def canvas1(word):
     wordLc = list(word)
     canvas = list('_' * len(word))
     noLetters = len(word) // 4
@@ -29,41 +23,81 @@ def guess(wordL, g):
         return False
 
 
-randint = random.randint(1,2000)
-query = "SELECT Name, District FROM city WHERE ID = " + str(randint)
-
-cursor.execute(query)
-
-lol = cursor.fetchall()
-dataset = lol[0]
-print(dataset)
-
-word = dataset[0]
-place = dataset[1]
 
 
-wordL = list(word)
-canvas = canvas(wordL)
-print(*canvas)
-print("District:",place)
-for i in range(10):
-    g = input("What is your guess ?")
-    if guess(wordL, g):
-        while wordL.count(g) != 0:
-            x = wordL.index(g)
-            canvas[x] = g
-            wordL[x] = ''
-        print(*canvas)
-        print(9 - i, 'chances left')
-    elif g == word:
-        print("CORRECT")
-        break
+database = sql.connect(host = "localhost", user = "root", passwd = "", database = "world")
+databaseLeader = sql.connect(host = "localhost", user="root", passwd= "", database = "autos")
+
+if database.is_connected():
+    print("Succesfully connected")
+
+cursor = database.cursor()
+cursorLeader = databaseLeader.cursor()
+
+Name = input("Enter your name")
+
+streak = True
+score = 0
+while streak == True:
+
+    randint = random.randint(1,2000)
+    query = "SELECT Name, District FROM city WHERE ID = " + str(randint)
+
+    cursor.execute(query)
+
+    lol = cursor.fetchall()
+    dataset = lol[0]
+    print(dataset)
+
+    word = dataset[0]
+    place = dataset[1]
+
+
+    wordL = list(word)
+    canvas = canvas1(wordL)
+    print(*canvas)
+    print("District:",place)
+    for i in range(10):
+        g = input("What is your guess ?")
+        if guess(wordL, g):
+            while wordL.count(g) != 0:
+                x = wordL.index(g)
+                canvas[x] = g
+                wordL[x] = ''
+            print(*canvas)
+            print(9 - i, 'chances left')
+        elif g == word:
+            print("CORRECT")
+            score+=1
+            break
+        else:
+            print("INCORRECT")
+            print(9 - i, 'chances left')
+            print(*canvas)
+        if canvas == list(word):
+            print("Correct")
+            score += 1
+            break
     else:
-        print("INCORRECT")
-        print(9 - i, 'chances left')
-        print(*canvas)
-    if canvas == list(word):
-        print("Correct")
-        break
-else:
-    print("The word is:-- ", word)
+        print("The word is:-- ", word)
+        print("Your score is", score)
+        streak = False
+
+
+query2 = "INSERT INTO bois (Name, Total, Best, Worst) values('"+ Name+"'," +str(score)+",0,0)"
+cursorLeader.execute(query2)
+databaseLeader.commit()
+
+query3 = "select * from bois order by Total DESC"
+cursorLeader.execute(query3)
+
+lol = cursorLeader.fetchall()
+
+print("\t 𝙇𝙚𝙖𝙙𝙚𝙧𝙗𝙤𝙖𝙧𝙙")
+rank = 1
+for i in lol:
+    print(rank,i[0],"\t", i[1])
+    rank+=1
+
+cursor.close()
+cursorLeader.close()
